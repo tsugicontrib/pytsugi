d7ff57a0114df19bc06c78444939949ec8b002b5
Charles Severance <csev@umich.edu>
2017-06-18 13:26:41 -0400

    Missing file

---
+++ tsugiclasses.py
@@ -0,0 +1,16 @@
+
+
+class TsugiLaunch():
+    """Holds the launch data for a Tsugi Launch
+    """
+    user = None
+    context = None
+    link = None
+    result = None
+    service = None
+    connection = None
+    complete = False
+    valid = False
+    message = None
+    detail = None
+    redirecturl = None
